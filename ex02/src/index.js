4cfb601d96bde6695fca7eac6d47c91753a8b8b6
Vasilije Vujovic <vasilijev7@gmail.com>
2022-03-30 10:28:22 +0200

    Komentari u JS

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+  //Only change code above this line
+  // This is a single line JavaScript comment
+  /* document.write("This is a multi-line comment!");
+  document.write("This is a multi-line comment!");
+  document.write("This is a multi-line comment!");
+  document.write("This is a multi-line comment!");
+  document.write("This is a multi-line comment!"); 
+  */
+  //Only change code above this line
+}
+
+main();
+
+module.exports = main;
