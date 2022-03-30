9727ddc02df9abbb3b1fef0b41024fd9b34454fc
Vasilije Vujovic <vasilijev7@gmail.com>
2022-03-30 11:57:17 +0200

    Odradjena 8 vjezba-sum

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo) {
+  //Only change code below this line
+  sum = numOne + numTwo;
+  //Only change code above this line
+  return sum;
+}
+
+console.log(main(15, 8));
+console.log(main(5, 28));
+console.log(main(150, 425));
+
+module.exports = main;
